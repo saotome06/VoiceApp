929a876a4feac90aa943e1ace579c22788dcad29
saotome06 <saotome03uhauha@gmail.com>
2024-05-11 18:15:23 +0900

    Initial Commit

---
+++ AICounseling/AICounselingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICounselingApp.swift
+//  AICounseling
+//
+//  Created by 早乙女琉真 on 2024/05/11.
+//
+
+import SwiftUI
+
+@main
+struct AICounselingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
