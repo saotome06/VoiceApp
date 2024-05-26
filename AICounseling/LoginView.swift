01383fd2032fde355d131254c6b67a85acdf66e6
saotome06 <saotome03uhauha@gmail.com>
2024-05-27 01:11:10 +0900

    Login追加

--- AICounseling/LoginView.swift
+++ AICounseling/LoginView.swift
@@ -1,23 +1,50 @@
 import SwiftUI
 import Speech
 import OpenAISwift
+import Supabase
+
+
+struct User: Decodable {
+    var id: Int
+    var user_email: String?
+    var nickname: String
+}
 
 struct LoginView: View {
+    
+    @State private var email: String = ""
+    @State private var loginSuccessMessage: String?
+    @State private var loginError: String?
+    @State private var users: [User] = []
+    @State private var errorMessage: String?
+    @State private var isLoggedIn = false // ログイン状態を管理する変数
+    
+    
+    private let supabaseURL = URL(string: "https://xxxxx.supabase.co")!
+    private let supabaseKey = "xxxxxxxx"
+    private var client: SupabaseClient
+    
+    init() {
+        client = SupabaseClient(supabaseURL: supabaseURL, supabaseKey: supabaseKey)
+    }
+    
     var body: some View {
-        // メールアドレスとパスワードを入力し,決定を押すと送信される
         VStack {
             Text("ログイン")
                 .font(.title)
                 .padding()
-            TextField("メールアドレス", text: .constant(""))
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            SecureField("パスワード", text: .constant(""))
+            TextField("メールアドレス", text: $email)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             Button(action: {
-                // ログイン処理
-            }) {
+                Task {
+                    await fetchUsers()
+                    checkLogin()
+                    print("isLoggedIn: \(isLoggedIn)") // デバッグメッセージ
+                    
+                }
+            })
+            {
                 Text("ログイン")
                     .fontWeight(.semibold)
                     .padding()
@@ -25,9 +52,67 @@ struct LoginView: View {
                     .background(Color.blue)
                     .cornerRadius(10)
             }
+            
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }else if let loginSuccessMessage = loginSuccessMessage {
+                Text(loginSuccessMessage)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            
+        }
+        .padding()
+        .fullScreenCover(isPresented: $isLoggedIn) {
+            TopView()
+        }
+        
+        
+    }
+    
+    private func fetchUsers() async {
+        do {
+            let response = try await client
+                .from("users")
+                .select()
+                .execute()
+            let data = response.data
+            let jsonDecoder = JSONDecoder()
+            let users = try jsonDecoder.decode([User].self, from: data)
+            DispatchQueue.main.async {
+                self.users = users
+                self.errorMessage = nil
+            }
+            
+        } catch {
+            print("Error fetching or decoding users: \(error)")
+            DispatchQueue.main.async {
+                self.errorMessage = "Error fetching users: \(error.localizedDescription)"
+            }
         }
     }
+    private func checkLogin() {
+        guard !email.isEmpty else {
+            errorMessage = "メールアドレスを入力してください"
+            return
+        }
+        
+        guard users.contains(where: { $0.user_email == email }) else {
+            errorMessage = "メールアドレスが見つかりません"
+            return
+        }
+        
+        loginSuccessMessage = "ログイン成功"
+        isLoggedIn = true
+        UserDefaults.standard.set(email, forKey: "user_email")
+        UserDefaults.standard.set(isLoggedIn, forKey: "isLoggedIn")
+        
+    }
 }
+
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
